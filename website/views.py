53db12f7d727843b333ae91e1ac89c060a89ba3d
Chris Hall <chall0311@gmail.com>
2024-03-11 08:56:08 -0400

    Added logic to register user.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import RegistrationForm
+
 
 def home(request):
     if request.method == 'POST':
@@ -15,6 +17,28 @@ def home(request):
             return redirect('home')
     else:
         return render(request, 'home.html', {})
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            # login user
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('home')
+
+    else:
+        form = RegistrationForm()
+        return render(request, 'register.html', {'form': form})
+
+    # The view website.views.register_user didn't return an HttpResponse object. It returned None instead.
+    return render(request, 'register.html', {'form': form})
+
 
 def logout_user(request):
     logout(request)
