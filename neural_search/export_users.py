6d5f8314545d0c3c9024e32fabd5f3c36b8b7a3b
Chris Hall <chall0311@gmail.com>
2024-03-27 18:56:24 -0400

    Created user export to json, vector script and upload to qdrant.

---
+++ neural_search/export_users.py
@@ -0,0 +1,39 @@
+import psycopg2
+import pandas as pd
+
+# Connect to your PostgreSQL database
+conn = psycopg2.connect(
+    dbname="tech_lister",
+    user="techlister",
+    password="techlister",
+    host="localhost",
+    port="5432"
+)
+
+# Construct the SQL query to join job listings with their skills
+query = """
+SELECT pi.first_name, pi.last_name, pi.city, pi.state, pi.about,
+       pi.profile_image, pi.user_id,  COALESCE(STRING_AGG(us.skill_name, ', '), '') AS skills
+FROM website_personalinformation AS pi
+LEFT JOIN website_userskill AS us ON pi.user_id = us.user_id
+GROUP BY pi.id
+"""
+
+# Read the SQL query result into a DataFrame
+df = pd.read_sql_query(query, conn)
+
+# Combine the 'about' column with the 'skills' column separated by '<br />'
+df['about'] = df['skills'] + '<br />' + df['about']
+
+# Drop the 'skills' column as it's no longer needed
+#df.drop(columns=['skills'], inplace=True)
+
+# Export DataFrame to a JSON file with each object on its own line
+df.to_json('user_listings.json', orient='records', lines=True)
+
+# Close connection
+conn.close()
+
+# Now try reading the JSON file back
+#df = pd.read_json("./job_listings.json", lines=True)
+#print(df)
---
+++ neural_search/init_user_upload.py
@@ -0,0 +1,31 @@
+# Import client library
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance
+import numpy as np
+import json
+
+qdrant_client = QdrantClient("http://localhost:6333")
+
+# related vectors need to be added to a collection
+qdrant_client.recreate_collection(
+    collection_name="userlistings",
+    vectors_config=VectorParams(size=768, distance=Distance.COSINE),
+)
+
+fd = open("./user_listings.json")
+
+# payload is now an iterator over startup data
+payload = map(json.loads, fd)
+
+# Load all vectors into memory, numpy array works as iterable for itself.
+# Other option would be to use Mmap, if you don't want to load all data into RAM
+vectors = np.load("./user_listings.npy")
+
+# upload data
+qdrant_client.upload_collection(
+    collection_name="userlistings",
+    vectors=vectors,
+    payload=payload,
+    ids=None,  # Vector ids will be assigned automatically
+    batch_size=256,  # How many vectors will be uploaded in a single request?
+)
