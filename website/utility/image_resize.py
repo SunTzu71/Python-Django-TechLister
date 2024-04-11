20c6d6e58b5171d40582c34231c6e9b93e7299af
Chris Hall <chall0311@gmail.com>
2024-04-10 22:38:15 -0400

    Fix image size and layout on portfolio pages
    The changes applied limit the image's size to 400x300 pixels on the user_page/portfolio.html and add_portfolio.html templates. On the image_resize.py function, images now save regardless of their original size, allowing original-sized ones to be stored without forcing a resize. Additionally, portfolio items container on the add portfolio page was resized for improved layout rendering.

--- website/utility/image_resize.py
+++ website/utility/image_resize.py
@@ -1,27 +1,27 @@
 from PIL import Image
 
 def image_resize(orig_image, file_name, img_height=300, img_width=300, new_file_name=None):
+    from io import BytesIO
+    from django.core.files.uploadedfile import InMemoryUploadedFile
+
     if orig_image.height > img_height or orig_image.width > img_width:
         orig_image.thumbnail((img_height, img_width))
 
-        # Create a BytesIO object to temporarily hold the resized image
-        from io import BytesIO
-        output_io = BytesIO()
+    # Create a BytesIO object to temporarily hold the resized image
+    output_io = BytesIO()
 
-        # Save the resized image to the BytesIO object
-        orig_image.save(output_io, format=orig_image.format)
+    # Save the image (resized or original) to the BytesIO object
+    orig_image.save(output_io, format=orig_image.format)
 
-        # Get the file name and extension from the original image
-        if new_file_name:
-            file_name = new_file_name
-        else:
-            file_name = file_name.name
+    # Get the file name and extension from the original image
+    if new_file_name:
+        file_name = new_file_name
+    else:
+        file_name = file_name.name
 
-        file_ext = file_name.split('.')[-1].lower()
+    file_ext = file_name.split('.')[-1].lower()
 
-        # Create a new InMemoryUploadedFile with the resized image data
-        from django.core.files.uploadedfile import InMemoryUploadedFile
-        resized_image = InMemoryUploadedFile(output_io, None, file_name, f'image/{file_ext}',
-                                             output_io.tell(), None)
-
-        return resized_image
+    # Create a new InMemoryUploadedFile with the image data
+    resized_image = InMemoryUploadedFile(output_io, None, file_name, f'image/{file_ext}',
+                                         output_io.tell(), None)
+    return resized_image
