6720e239d4157a639b86d6993ab96f2d344bdc00
Chris Hall <chall0311@gmail.com>
2024-04-17 08:38:41 -0400

    Add user access validation to message reply
    Updated the reply_message view in the messaging module to include an additional check. A validation has been introduced to ensure that the user making a reply has access rights to the original message. This helps manage cases where the reply-requesting user is not the intended recipient of the original message, redirecting them to a 'restricted access' page in such scenarios.

--- messaging/views.py
+++ messaging/views.py
@@ -28,19 +28,24 @@ def new_message(request, user_id):
 @login_required
 def reply_message(request, msg_id, user_id):
     if request.method == 'POST':
-        # need to check to make sure request user is the to_user
-        # and is part of the message they can reply to
-        form = ReplyMessageForm(request.POST)
-        context = {'form': form, 'msg_id': msg_id, 'user_id': user_id}
-        if form.is_valid():
-            add_message = form.save(commit=False)
-            add_message.from_user = request.user
-            add_message.to_user = User.objects.get(pk=user_id)
-            add_message.message_id = msg_id
-            add_message.save()
-            return render(request, 'messages/message-sent.html')
-        else:
-            return render(request, 'reply_message.html', context)
+        try:
+            # check if user has access to reply
+            check_user = Message.objects.get(id=msg_id, to_user=request.user)
+
+            form = ReplyMessageForm(request.POST)
+            context = {'form': form, 'msg_id': msg_id, 'user_id': user_id}
+            if form.is_valid():
+                add_message = form.save(commit=False)
+                add_message.from_user = request.user
+                add_message.to_user = User.objects.get(pk=user_id)
+                add_message.message_id = msg_id
+                add_message.save()
+                return render(request, 'messages/message-sent.html')
+            else:
+                return render(request, 'reply_message.html', context)
+        except Message.DoesNotExist:
+            # Handle the case where the user in request.user is not in the to_user of Message
+            return redirect('restricted_access')
     else:
         form = ReplyMessageForm()
         context = {'form': form, 'msg_id': msg_id, 'user_id': user_id}
