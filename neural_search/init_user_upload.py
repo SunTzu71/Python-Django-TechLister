332fb58b84cbd4e50631e15624dc43380da67475
Chris Hall <chall0311@gmail.com>
2024-04-17 13:58:05 -0400

    Update Qdrant client and remove description title in job listings
    Qdrant client in init_user_upload.py, init_job_upload.py and neural_searcher.py is updated to use a remote endpoint with an API key instead of the localhost. Additionally, the title 'Description' in job_listings.html template is removed to simplify the job listings display.

--- neural_search/init_user_upload.py
+++ neural_search/init_user_upload.py
@@ -4,7 +4,11 @@ from qdrant_client.models import VectorParams, Distance
 import numpy as np
 import json
 
-qdrant_client = QdrantClient("http://localhost:6333")
+#qdrant_client = QdrantClient("http://localhost:6333")
+qdrant_client = QdrantClient(
+    url="https://a70aa4ee-b102-4f72-87bc-2d84095c40fd.us-east4-0.gcp.cloud.qdrant.io:6333",
+    api_key="LBtuvaZRmX_kTUZJTTaj2D7DOqSl8DoipFBnMqWH9C16BC15u7MqlQ",
+)
 
 # related vectors need to be added to a collection
 qdrant_client.recreate_collection(
